5879566b4e99f04ae31f2e68f9c67affa88df812
Times Trading <timesgacc@gmail.com>
2025-10-17 09:22:00 +0800

    update

--- pendingmaterial.sql
+++ pendingmaterial.sql
@@ -1,5 +1,21 @@
-select transaction_header_id, source_line_id, source_header_id, inventory_item_id, transaction_quantity, transaction_uom, subinventory_code, transaction_source_id, transaction_type_id, transaction_reference, trx_source_line_id, ship_to_location_id, 
-    transfer_subinventory, error_explanation
-from mtl_transactions_interface
-where process_flag = 3
-order by transaction_date
+select
+    transaction_header_id,
+    source_line_id,
+    source_header_id,
+    inventory_item_id,
+    transaction_quantity,
+    transaction_uom,
+    subinventory_code,
+    transaction_source_id,
+    transaction_type_id,
+    transaction_reference,
+    trx_source_line_id,
+    ship_to_location_id,
+    transfer_subinventory,
+    error_explanation
+from
+    mtl_transactions_interface
+where
+    process_flag = 3
+order by
+    transaction_date
--- unprocessedmaterial.sql
+++ unprocessedmaterial.sql
@@ -1,5 +1,27 @@
-select transaction_header_id, transaction_temp_id, transaction_mode, inventory_item_id, subinventory_code, locator_id, transaction_quantity, transaction_uom, transaction_type_id, transaction_source_id, transaction_source_name, transaction_date, 
-    transaction_reference, trx_source_line_id, transfer_subinventory, transfer_to_location, process_flag, error_explanation, pick_slip_number
-from mtl_material_transactions_temp
-where process_flag = 'E' and organization_id = 121
-order by transaction_date
+select
+    transaction_header_id,
+    transaction_temp_id,
+    transaction_mode,
+    inventory_item_id,
+    subinventory_code,
+    locator_id,
+    transaction_quantity,
+    transaction_uom,
+    transaction_type_id,
+    transaction_source_id,
+    transaction_source_name,
+    transaction_date,
+    transaction_reference,
+    trx_source_line_id,
+    transfer_subinventory,
+    transfer_to_location,
+    process_flag,
+    error_explanation,
+    pick_slip_number
+from
+    mtl_material_transactions_temp
+where
+    process_flag = 'E'
+    and organization_id = 121
+order by
+    transaction_date
